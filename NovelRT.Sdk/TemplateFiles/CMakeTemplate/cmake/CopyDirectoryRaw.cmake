a304d4649678fb81c9a089373dc3710744e54583
Ken Johnson <k.rjohnson94@outlook.com>
2022-02-25 13:01:44 -0500

    Updated linux copying to be escaped and quoted properly

--- NovelRT.Sdk/TemplateFiles/CMakeTemplate/cmake/CopyDirectoryRaw.cmake
+++ NovelRT.Sdk/TemplateFiles/CMakeTemplate/cmake/CopyDirectoryRaw.cmake
@@ -6,8 +6,8 @@
         \"$<TARGET_FILE_DIR:${target}>\")
   else() 
     add_custom_command(TARGET ${target} POST_BUILD COMMAND cp -r
-        ${directory}
-        $<TARGET_FILE_DIR:${target}>)
+        \"${directory}\"
+        \"$<TARGET_FILE_DIR:${target}>\")
   endif()
   
 endfunction()
